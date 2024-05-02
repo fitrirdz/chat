67dbc25a0e3f6765b544449841e6fd09d328b124
Fitri R Dewi <fitri@softwareseni.com>
2024-05-02 11:27:57 +0700

    Use subsciption on client

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -8,15 +8,12 @@ function Chat({ user }) {
 
   const handleSend = async (text) => {
     const message = await addMessage(text);
-    console.log('Message added:', message);
   };
 
   return (
-    <section className="section">
-      <div className="container">
-        <h1 className="title is-4">
-          {`Chatting as ${user}`}
-        </h1>
+    <section className='section'>
+      <div className='container'>
+        <h1 className='title is-4'>{`Chatting as ${user}`}</h1>
         <MessageList user={user} messages={messages} />
         <MessageInput onSend={handleSend} />
       </div>
